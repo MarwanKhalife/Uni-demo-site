58deafd18c773935eecfc6e92359c6d81445b8b9
marwankhalife <maljeburi@gmail.com>
2022-04-10 12:09:58 +0200

    push to GH

---
+++ form-handeler.php
@@ -0,0 +1,25 @@
+<?php
+$name = $_POST['name']
+$visitor_email = $_POST['email']
+$subject = $_POST['subject']
+$message = $_POST['message']
+
+$email_from = 'info@TheBestUniversity.world'
+
+$email_subject = 'New Message from University Website Demo'
+
+$email_body = "User Name: $name.\n".
+                "User Email: $visitor_email.\n".
+                "Subject: $subject.\n".
+                "User Message: $message .\n";
+
+$to = 'info@TheBestUniversity.world';
+
+$headers = "From: $email_from \r\n";
+
+$headers .= "Reply-to: $visitor_email \r\n";
+
+mail($to,$email_subject,$email_body,$headers);
+header("location: contact.html");
+
+?>  
